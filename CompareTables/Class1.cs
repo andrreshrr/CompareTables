151a559d5b7faf08a9fd3538ff9afaf88d5a0711
rreshrr@gmail.com <rreshrr@gmail.com>
2021-07-06 13:56:52 +0300

    Добавьте файлы проекта.

---
+++ CompareTables/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace CompareTables
+{
+    public class Class1
+    {
+    }
+}
---
+++ CompareTables/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CompareTables
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string firstFile = null;
+        private string secondFile = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void File1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog(); //открываем меню выбора файла
+            ofd.DefaultExt = "*.xls;*.xlsx";
+            ofd.Filter = "Microsoft Excel (*.xls*)|*.xls*";
+            ofd.Title = "Выберите 1-й файл для сверки";
+            
+            if (ofd.ShowDialog() != DialogResult) //если юзер не выбрал файл
+            //{
+              //  MessageBox.Show("Вы не выбрали файл для открытия", "Загрузка данных...", MessageBoxButton.OK, MessageBoxImage.Error);
+               // return;
+            //}
+            firstFile = ofd.FileName; // - путь к файлу
+            File1.Content = ofd.FileName.Substring(ofd.FileName.LastIndexOf('\\') + 1);
+        }
+
+        private void File2_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog(); //открываем меню выбора файла
+            ofd.DefaultExt = "*.xls;*.xlsx";
+            ofd.Filter = "Microsoft Excel (*.xls*)|*.xls*";
+            ofd.Title = "Выберите 2-й файл для сверки";
+
+             if (ofd.ShowDialog() != DialogResult) //если юзер не выбрал файл
+            //{
+            //    MessageBox.Show("Вы не выбрали файл для открытия", "Загрузка данных...", MessageBoxButton.OK, MessageBoxImage.Error);
+            //    return;
+            //}
+            secondFile = ofd.FileName; // - путь к файлу
+            
+            
+            File2.Content = ofd.FileName.Substring(ofd.FileName.LastIndexOf('\\')+1);
+      
+
+
+        }
+
+        private void Action_Click(object sender, RoutedEventArgs e)
+        {
+            if ((firstFile == null) || (secondFile == null))
+            {
+                MessageBox.Show("Вы не выбрали файлs", "Загрузка данных...", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Excel.Application xlApp = new Excel.Application(); //создаем excel-приложение
+            Excel.Workbook xlWB1; //excel-файл
+            Excel.Worksheet xlWS1; //excel-лист
+            xlWB1 = xlApp.Workbooks.Open(firstFile); //инициализируем переменные нашими фйлами
+            xlWS1 =(Excel.Worksheet) xlWB1.Worksheets["Лист1"]; //или как там? кароче, индекс - название листа
+
+
+     
+        }
+    }
+}
