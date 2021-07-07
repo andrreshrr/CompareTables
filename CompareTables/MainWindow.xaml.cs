e6873b1db619606352ed1fa98408b0b891caf3d4
rreshrr@gmail.com <rreshrr@gmail.com>
2021-07-07 18:02:36 +0300

    Драг-н-дроп в массы!

--- CompareTables/MainWindow.xaml.cs
+++ CompareTables/MainWindow.xaml.cs
@@ -36,9 +36,9 @@ namespace CompareTables
             ofd.Filter = "Microsoft Excel (*.xls*)|*.xls*";
             ofd.Title = "Выберите 1-й файл для сверки";
             
-            if (ofd.ShowDialog() != DialogResult) //если юзер не выбрал файл
+            if (!(bool)ofd.ShowDialog()) //если юзер не выбрал файл
             {
-                MessageBox.Show("Вы не выбрали файл для открытия", "Загрузка данных...", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Вы не выбрали файл для загрузки", "Загрузка данных...", MessageBoxButton.OK, MessageBoxImage.Warning);
                 return;
             }
             firstFile = ofd.FileName; // - путь к файлу
@@ -52,25 +52,28 @@ namespace CompareTables
             ofd.Filter = "Microsoft Excel (*.xls*)|*.xls*";
             ofd.Title = "Выберите 2-й файл для сверки";
 
-             if (ofd.ShowDialog() != DialogResult) //если юзер не выбрал файл
+             if (!(bool)ofd.ShowDialog()) //если юзер не выбрал файл
             {
-                MessageBox.Show("Вы не выбрали файл для открытия", "Загрузка данных...", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Вы не выбрали файл для загрузки", "Загрузка данных...", MessageBoxButton.OK, MessageBoxImage.Warning);
                 return;
             }
             secondFile = ofd.FileName; // - путь к файлу
             
             
+
             File2.Content = ofd.FileName.Substring(ofd.FileName.LastIndexOf('\\')+1);
       
 
 
         }
 
+
+        //нажатие на кнопку когда все файлы загружены!!
         private void Action_Click(object sender, RoutedEventArgs e)
         {
             if ((firstFile == null) || (secondFile == null))
             {
-                MessageBox.Show("Вы не выбрали файлs", "Загрузка данных...", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Вы не выбрали файлы", "Загрузка данных...", MessageBoxButton.OK, MessageBoxImage.Warning);
                 return;
             }
 
@@ -80,8 +83,104 @@ namespace CompareTables
             xlWB1 = xlApp.Workbooks.Open(firstFile); //инициализируем переменные нашими фйлами
             xlWS1 =(Excel.Worksheet) xlWB1.Worksheets["Лист1"]; //или как там? кароче, индекс - название листа
 
+            Excel.Workbook xlWB2; //excel-файл
+            Excel.Worksheet xlWS2; //excel-лист
+            xlWB2 = xlApp.Workbooks.Open(secondFile); //инициализируем переменные нашими фйлами
+            xlWS2 = (Excel.Worksheet)xlWB2.Worksheets["Лист1"]; //или как там? кароче, индекс - название листа
 
-     
+
+
         }
+
+        //начало описания драгндропа
+        private void field1Enter(object sender, DragEventArgs e) 
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+                Field1.Fill = Brushes.White;
+            }
+            //File1.Visibility = Visibility.Collapsed;
+        }
+
+        private void field2Enter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+                Field2.Fill = Brushes.White;
+            }
+            
+           // File2.Visibility = Visibility.Hidden;
+        }
+
+        private void field1Leave(object sender, DragEventArgs e)
+        {
+            Field1.Fill = Brushes.Silver;
+            //File1.Visibility = Visibility.Visible;
+        }
+        private void field2Leave(object sender, DragEventArgs e)
+        {
+            Field2.Fill = Brushes.Silver;
+            //File2.Visibility = Visibility.Visible;
+        }
+
+        private void File2_Drop(object sender, DragEventArgs e)
+        {
+            var rr = e.Data.GetFormats();
+            var same = e.Data.GetData("FileNameW");
+            string name = ((string[])same)[0];
+            string ext = null;
+            if (name.IndexOf('.') != -1)
+            {
+                ext = name.Substring(name.LastIndexOf('.') + 1, name.Length - name.LastIndexOf('.') - 1); //получение расширения                                                                                                //
+            }
+            else
+            ext = "folder";
+            if ((ext == "xls") || (ext == "xlsx"))
+            {
+                secondFile = name;
+                File2.Content = name.Substring(name.LastIndexOf('\\') + 1); //имя файла
+            } else
+            {
+
+                MessageBox.Show("Загруженный файл имеет некорректное расширение!\nПоддерживаемые расширения: xls, xlsx.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+          
+            }
+
+            Field2.Fill = Brushes.Silver;
+
+
+        }
+
+        private void File1_Drop(object sender, DragEventArgs e)
+        {
+            var rr = e.Data.GetFormats();
+            var same = e.Data.GetData("FileNameW");
+            string name = ((string[])same)[0];
+            string ext = null;
+            if (name.IndexOf('.') != -1)
+            {
+                ext = name.Substring(name.LastIndexOf('.') + 1, name.Length - name.LastIndexOf('.') - 1); //получение расширения                                                                                                //
+            }
+            else
+                ext = "folder";
+            if ((ext == "xls") || (ext == "xlsx"))
+            {
+                firstFile = name;
+                File1.Content = name.Substring(name.LastIndexOf('\\') + 1); //имя файла
+            }
+            else
+            {
+
+                MessageBox.Show("Загруженный файл имеет некорректное расширение!\nПоддерживаемые расширения: xls, xlsx.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+
+            Field1.Fill = Brushes.Silver;
+
+
+        }
+
     }
 }
